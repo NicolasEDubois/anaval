15d21e78711525c4d812cdffc1020a1f938d2f9a
Nicolas Dubois <nicolas.dubois@uclouvain.be>
2017-03-08 18:35:18 +0100

    Pass check 0 error, O warning, 0 notes

--- R/calibration.R
+++ R/calibration.R
@@ -14,28 +14,29 @@
 #' @return MyDOE (the output) is a data.frame with 6 columns (RunTechnician, ConcentrationLabel, ConcentrationValue, CalibCurve, ReplicateNumber,
 #'Responses). Responses column contains onlys NaN at this time and will be filled by simulation or with the experimental results.
 #' @examples
-#' calib_doe(nRun = 2, nCalibCurvesPerRun = 3, nrepCalib = 5, ConcVect = c(0,50, 100, 125, 150, 175, 200))
+#' calib_doe(nRun = 2, nCalibCurvesPerRun = 3, nrepCalib = 5,
+#'  ConcVect = c(0,50,100,125,150,175,200))
 #' @export
-calib_doe <- function(nRun = 3, nCalibCurvesPerRun = 2, nrepCalib = 3, ConcVect = c(0, 50, 100, 125, 150,
-    175, 200)) {
+calib_doe <- function(nRun = 3, nCalibCurvesPerRun = 2, nrepCalib = 3, ConcVect = c(0, 50, 
+    100, 125, 150, 175, 200)) {
     # DOE
-    factorlist <- c("RunTechnician", "ConcentrationLabel", "ConcentrationValue", "CalibCurve", "ReplicateNumber",
-        "Response")
+    factorlist <- c("RunTechnician", "ConcentrationLabel", "ConcentrationValue", "CalibCurve", 
+        "ReplicateNumber", "Response")
     f1 <- paste("Run", sprintf("%03d", c(1:nRun)), sep = "")  # 1st factor levels
-
+    
     f2 <- ConcVect  # 2nd factor levels
-
+    
     k1 <- length(f1)  # number of 1st factors
     k2 <- length(f2)  # number of 2nd factors
-
+    
     f3 <- c(1:nCalibCurvesPerRun)
-
+    
     n <- nrepCalib * nCalibCurvesPerRun
-
+    
     MyDOE <- matrix(NaN, nrow = k1 * k2 * n, ncol = length(factorlist))
-
+    
     colnames(MyDOE) <- c(factorlist)
-
+    
     # Simulation of the response (signal)
     rowId <- 1
     for (i in c(1:k1)) {
@@ -43,9 +44,11 @@ calib_doe <- function(nRun = 3, nCalibCurvesPerRun = 2, nrepCalib = 3, ConcVect 
             for (l in c(1:nrepCalib)) {
                 for (j in c(1:k2)) {
                   MyDOE[rowId, "RunTechnician"] = f1[i]
-                  MyDOE[rowId, "ConcentrationLabel"] = paste(sprintf("%03d", as.numeric(f2[j])), sep = "")
+                  MyDOE[rowId, "ConcentrationLabel"] = paste(sprintf("%03d", as.numeric(f2[j])), 
+                    sep = "")
                   MyDOE[rowId, "ConcentrationValue"] = f2[j]
-                  MyDOE[rowId, "CalibCurve"] = paste(f1[i], sprintf("%02d", as.numeric(f3[k])), sep = "_")
+                  MyDOE[rowId, "CalibCurve"] = paste(f1[i], sprintf("%02d", as.numeric(f3[k])), 
+                    sep = "_")
                   MyDOE[rowId, "ReplicateNumber"] = paste(sprintf("%02d", l, sep = ""))
                   MyDOE[rowId, "Response"] = NaN
                   rowId <- rowId + 1
--- tests/testthat/test_calibration.R
+++ tests/testthat/test_calibration.R
@@ -1,21 +1,21 @@
 context("calibration")
 
 test_that("DOE output is a data.frame", {
-    expect_is(calib_doe(nRun = 3, nCalibCurvesPerRun = 2, nrepCalib = 3, ConcVect = c(0, 50, 100, 125,
-        150, 175, 200)), "data.frame")
+    expect_is(calib_doe(nRun = 3, nCalibCurvesPerRun = 2, nrepCalib = 3, ConcVect = c(0, 
+        50, 100, 125, 150, 175, 200)), "data.frame")
 })
 
 test_that("DOE data.frame has the right number of rows", {
-    expect_equal(dim(calib_doe(nRun = 3, nCalibCurvesPerRun = 1, nrepCalib = 1, ConcVect = c(0)))[1],
+    expect_equal(dim(calib_doe(nRun = 3, nCalibCurvesPerRun = 1, nrepCalib = 1, ConcVect = c(0)))[1], 
         3)
-    expect_equal(dim(calib_doe(nRun = 1, nCalibCurvesPerRun = 2, nrepCalib = 1, ConcVect = c(0)))[1],
+    expect_equal(dim(calib_doe(nRun = 1, nCalibCurvesPerRun = 2, nrepCalib = 1, ConcVect = c(0)))[1], 
         2)
-    expect_equal(dim(calib_doe(nRun = 1, nCalibCurvesPerRun = 1, nrepCalib = 4, ConcVect = c(0)))[1],
+    expect_equal(dim(calib_doe(nRun = 1, nCalibCurvesPerRun = 1, nrepCalib = 4, ConcVect = c(0)))[1], 
         4)
-    expect_equal(dim(calib_doe(nRun = 1, nCalibCurvesPerRun = 1, nrepCalib = 1, ConcVect = c(0, 100, 200)))[1],
-        3)
-    expect_equal(dim(calib_doe(nRun = 2, nCalibCurvesPerRun = 3, nrepCalib = 5, ConcVect = c(0, 50, 100,
-        125, 150, 175, 200)))[1], 210)
-    expect_equal(dim(calib_doe(nRun = 2, nCalibCurvesPerRun = 3, nrepCalib = 5, ConcVect = c(0, 50, 100,
-        125, 150, 175, 200)))[2], 6)
+    expect_equal(dim(calib_doe(nRun = 1, nCalibCurvesPerRun = 1, nrepCalib = 1, ConcVect = c(0, 
+        100, 200)))[1], 3)
+    expect_equal(dim(calib_doe(nRun = 2, nCalibCurvesPerRun = 3, nrepCalib = 5, ConcVect = c(0, 
+        50, 100, 125, 150, 175, 200)))[1], 210)
+    expect_equal(dim(calib_doe(nRun = 2, nCalibCurvesPerRun = 3, nrepCalib = 5, ConcVect = c(0, 
+        50, 100, 125, 150, 175, 200)))[2], 6)
 })
