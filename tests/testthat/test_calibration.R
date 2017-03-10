afa0cdd543949c75457a27f68227343846c6873c
Nicolas Dubois <nicolas.dubois@uclouvain.be>
2017-03-10 14:20:07 +0100

    Testthat comparison with ref DOE

--- tests/testthat/test_calibration.R
+++ tests/testthat/test_calibration.R
@@ -1,21 +1,27 @@
 context("calibration")
 
 test_that("DOE output is a data.frame", {
-    expect_is(calib_doe(nRun = 3, nCalibCurvesPerRun = 2, nrepCalib = 3, ConcVect = c(0, 
+    expect_is(calib_doe(nRun = 3, nCalibCurvesPerRun = 2, nrepCalib = 3, ConcVect = c(0,
         50, 100, 125, 150, 175, 200)), "data.frame")
 })
 
 test_that("DOE data.frame has the right number of rows", {
-    expect_equal(dim(calib_doe(nRun = 3, nCalibCurvesPerRun = 1, nrepCalib = 1, ConcVect = c(0)))[1], 
+    expect_equal(dim(calib_doe(nRun = 3, nCalibCurvesPerRun = 1, nrepCalib = 1, ConcVect = c(0)))[1],
         3)
-    expect_equal(dim(calib_doe(nRun = 1, nCalibCurvesPerRun = 2, nrepCalib = 1, ConcVect = c(0)))[1], 
+    expect_equal(dim(calib_doe(nRun = 1, nCalibCurvesPerRun = 2, nrepCalib = 1, ConcVect = c(0)))[1],
         2)
-    expect_equal(dim(calib_doe(nRun = 1, nCalibCurvesPerRun = 1, nrepCalib = 4, ConcVect = c(0)))[1], 
+    expect_equal(dim(calib_doe(nRun = 1, nCalibCurvesPerRun = 1, nrepCalib = 4, ConcVect = c(0)))[1],
         4)
-    expect_equal(dim(calib_doe(nRun = 1, nCalibCurvesPerRun = 1, nrepCalib = 1, ConcVect = c(0, 
+    expect_equal(dim(calib_doe(nRun = 1, nCalibCurvesPerRun = 1, nrepCalib = 1, ConcVect = c(0,
         100, 200)))[1], 3)
-    expect_equal(dim(calib_doe(nRun = 2, nCalibCurvesPerRun = 3, nrepCalib = 5, ConcVect = c(0, 
+    expect_equal(dim(calib_doe(nRun = 2, nCalibCurvesPerRun = 3, nrepCalib = 5, ConcVect = c(0,
         50, 100, 125, 150, 175, 200)))[1], 210)
-    expect_equal(dim(calib_doe(nRun = 2, nCalibCurvesPerRun = 3, nrepCalib = 5, ConcVect = c(0, 
+    expect_equal(dim(calib_doe(nRun = 2, nCalibCurvesPerRun = 3, nrepCalib = 5, ConcVect = c(0,
         50, 100, 125, 150, 175, 200)))[2], 6)
+})
+
+test_that("The DOE data.frame matches the reference data.frame", {
+  data("ref_calib_doe")
+  test_doe = calib_doe(nRun = 2, nCalibCurvesPerRun = 3, nrepCalib = 5, ConcVect = c(0, 50, 100, 125, 150, 175, 200))
+  expect_identical(test_doe, ref_calib_doe)
 })
